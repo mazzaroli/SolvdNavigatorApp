52e46a35bbb1c04739ae85f5d1543efe8728d723
FausVillegas <faustovillegas2015@gmail.com>
2024-01-29 20:01:34 -0300

    Feature: Introduce Bus Model
    Description:
    This commit introduces the Bus class, representing a bus entity with attributes id and lineName. The class includes accessor methods, equals, hashCode, and toString implementations for proper functionality and representation.
    
    Context:
    The Bus class is designed to model basic information about a bus, including its id and line name.
    
    Body:
    - Added Bus class with attributes: id, lineName.
    - Implemented accessor methods for id and lineName.
    - Overridden equals, hashCode, and toString methods for proper object comparison and representation.
    
    Related Issue:
    - https://solvd-navigator-app.atlassian.net/browse/ST2-9
    
    Co-authored-by: fvillegas.laba@solvd.com

--- src/main/java/com/solvd/database/model/Bus.java
+++ src/main/java/com/solvd/database/model/Bus.java
@@ -1,9 +1,50 @@
 package com.solvd.database.model;
 
-public class Bus {
-//    manana
-//    hacer test json
-//    hacer test jaxb
+import java.util.Objects;
 
-// y despues me borro ig por un mes
+public class Bus{
+    private int id;
+    private String lineName;
+
+    public Bus(int id, String lineName) {
+        this.id = id;
+        this.lineName = lineName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLineName() {
+        return lineName;
+    }
+
+    public void setLineName(String lineName) {
+        this.lineName = lineName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bus bus = (Bus) o;
+        return id == bus.id && Objects.equals(lineName, bus.lineName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lineName);
+    }
+
+    @Override
+    public String toString() {
+        return "Bus{" +
+                "id=" + id +
+                ", lineName='" + lineName + '\'' +
+                '}';
+    }
 }
