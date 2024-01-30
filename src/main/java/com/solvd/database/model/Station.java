925ca1c9f5efcce254af6387609b48314e41ff39
Mazzaroli <carlitosmazaroli@gmail.com>
2024-01-30 14:11:45 -0300

    Docs: Add Station Entity Class
    Description:
    This commit introduces the `Station` entity class to the project. The `Station` class represents a station entity in the system and includes properties for the station ID, name, and coordinates.
    
    Body:
    - Added the `Station` entity class to the project.
    - Includes properties for the station ID (`id`), name (`name`), and coordinates (`coordinateX` and `coordinateY`).
    - Implemented getters and setters for the properties.
    - Overridden the `toString` method for easy representation of the entity.

--- src/main/java/com/solvd/database/model/Station.java
+++ src/main/java/com/solvd/database/model/Station.java
@@ -1,4 +1,50 @@
 package com.solvd.database.model;
 
 public class Station {
+    private int id;
+    private String name;
+    private double coordinateX;
+    private double coordinateY;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void setCoordinateX(double coordinateX) {
+        this.coordinateX = coordinateX;
+    }
+
+    public double getCoordinateY() {
+        return coordinateY;
+    }
+
+    public void setCoordinateY(double coordinateY) {
+        this.coordinateY = coordinateY;
+    }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", coordinateX=" + coordinateX +
+                ", coordinateY=" + coordinateY +
+                '}';
+    }
 }
