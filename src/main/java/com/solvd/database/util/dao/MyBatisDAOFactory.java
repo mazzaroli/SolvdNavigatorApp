c5e383638aefdf3f71bf5cd5b18ac1ba6ddeb414
Mazzaroli <carlitosmazaroli@gmail.com>
2024-01-30 18:43:03 -0300

    Refactor: Update MyBatisDAOFactory
    Description:
    This commit refactors the `MyBatisDAOFactory` class to include new cases for the `CONNECTION` and `ROUTE` types, returning instances of `ConnectionDAOImpl` and `RouteDAOImpl` respectively. This ensures the factory can create DAO instances for the `Connection` and `Route` entities.
    
    Body:
    - Added cases for `CONNECTION` and `ROUTE` types in the switch statement of `createDAO` method.
    - The `CONNECTION` case returns a new instance of `ConnectionDAOImpl`.
    - The `ROUTE` case returns a new instance of `RouteDAOImpl`.

--- src/main/java/com/solvd/database/util/dao/MyBatisDAOFactory.java
+++ src/main/java/com/solvd/database/util/dao/MyBatisDAOFactory.java
@@ -2,6 +2,8 @@ package com.solvd.database.util.dao;
 
 import com.solvd.database.dao.IBaseDao;
 import com.solvd.database.dao.mybatis.BusDAOImpl;
+import com.solvd.database.dao.mybatis.ConnectionDAOImpl;
+import com.solvd.database.dao.mybatis.RouteDAOImpl;
 import com.solvd.database.dao.mybatis.StationDAOImpl;
 import com.solvd.database.util.abstractFactory.AbstractFactory;
 import com.solvd.enums.DAOType;
@@ -26,6 +28,10 @@ public class MyBatisDAOFactory extends AbstractFactory {
                 return new BusDAOImpl();
             case STATION:
                 return new StationDAOImpl();
+            case CONNECITION:
+                return new ConnectionDAOImpl();
+            case ROUTE:
+                return new RouteDAOImpl();
             default:
                 logger.error("Unexpected value: " + dao);
                 return null;
