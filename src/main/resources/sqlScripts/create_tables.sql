578938c7dc2d514e985badca8bf180b7facf0af8
Mazzaroli <carlitosmazaroli@gmail.com>
2024-01-30 14:40:42 -0300

    Refactor: Create Navigator Database Schema
    Description:
    This commit introduces the SQL script for creating the Navigator database schema. The schema includes tables for Stations, Connections, Buses, and Routes. Notable features:
    - Stations table with columns: id, station_name, coordinate_x, coordinate_y.
    - Connections table with columns: id, origin_station_id, destination_station_id, and foreign keys referencing Stations.
    - Buses table with columns: id, line_name.
    - Routes table with columns: id, bus_id, route_order, station_id, and foreign keys referencing Buses and Stations with CASCADE DELETE.
    
    Body:
    - Created the `navigator` database.
    - Defined the Stations table with columns and constraints.
    - Defined the Connections table with columns and foreign key constraints.
    - Defined the Buses table with columns.
    - Defined the Routes table with columns and foreign key constraints using CASCADE DELETE.

--- src/main/resources/sqlScripts/create_tables.sql
+++ src/main/resources/sqlScripts/create_tables.sql
@@ -1,39 +1,36 @@
--- Create mavigator database
+-- Create navigator database
 CREATE DATABASE IF NOT EXISTS navigator;
 USE navigator;
 
 -- Stations Table
 CREATE TABLE Stations (
-    id INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    coordinateX DOUBLE NOT NULL,
-    coordinateY DOUBLE NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    station_name VARCHAR(255) NOT NULL,
+    coordinate_x DOUBLE NOT NULL,
+    coordinate_y DOUBLE NOT NULL
 );
 
 -- Connections Table
 CREATE TABLE Connections (
-    id INT PRIMARY KEY,
-    id_origin_station INT,
-    id_destination_station INT,
-    FOREIGN KEY (id_origin_station) REFERENCES Stations(id),
-    FOREIGN KEY (id_destination_station) REFERENCES Stations(id)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    origin_station_id INT,
+    destination_station_id INT,
+    FOREIGN KEY (origin_station_id) REFERENCES Stations(id),
+    FOREIGN KEY (destination_station_id) REFERENCES Stations(id)
 );
-
 
 -- Buses Table
 CREATE TABLE Buses (
-    id INT PRIMARY KEY,
-    lineName VARCHAR(255) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    line_name VARCHAR(255) NOT NULL
 );
 
-
--- Routes Table
+-- Routes Table with CASCADE DELETE
 CREATE TABLE Routes (
-    id INT PRIMARY KEY,
-    id_bus INT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    bus_id INT,
     route_order INT NOT NULL,
-    id_station INT,
-    FOREIGN KEY (id_bus) REFERENCES Buses(id),
-    FOREIGN KEY (id_station) REFERENCES Stations(id)
+    station_id INT,
+    FOREIGN KEY (bus_id) REFERENCES Buses(id) ON DELETE CASCADE,
+    FOREIGN KEY (station_id) REFERENCES Stations(id)
 );
-
