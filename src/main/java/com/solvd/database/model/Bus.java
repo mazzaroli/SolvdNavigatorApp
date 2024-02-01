328a93ab75d3f621cb4507ca3281f74d9ea245bc
Mazzaroli <carlitosmazaroli@gmail.com>
2024-01-31 22:32:39 -0300

    Refactor: Enhance Bus Class
    Description:
    This refactor introduces additional attributes and methods to enhance the functionality and representation of the `Bus` class.
    
    Changes:
    - Added a new attribute `conexionId` of type `int` to store the connection ID associated with the bus.
    - Implemented getter and setter methods for the new attribute.
    - Introduced a constructor to initialize the `Bus` object.
    - Updated the `toString` method to include the new attribute in the string representation.
    - Added `hashCode` and `equals` methods, although they currently delegate to the superclass implementations.
    
    Context:
    The changes aim to provide more comprehensive information about the bus, including its associated connection ID. This can be useful for scenarios where a bus is directly linked to a specific connection.

--- src/main/java/com/solvd/database/model/Bus.java
+++ src/main/java/com/solvd/database/model/Bus.java
@@ -3,6 +3,8 @@ package com.solvd.database.model;
 public class Bus {
     private Integer id;
     private String lineName;
+    private int conexionId;
+
 
     public Integer getId() {
         return id;
@@ -20,11 +22,34 @@ public class Bus {
         this.lineName = lineName;
     }
 
+    public int getConexionId() {
+        return conexionId;
+    }
+
+    public void setConexionId(int conexionId) {
+        this.conexionId = conexionId;
+    }
+
+    public Bus() {
+        super();
+    }
+
     @Override
     public String toString() {
         return "Bus{" +
                 "id=" + id +
                 ", lineName='" + lineName + '\'' +
+                ", conexionId=" + conexionId +
                 '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
