e19b641d89ec76889aa84ebbe7a309c4752839c8
Mazzaroli <carlitosmazaroli@gmail.com>
2024-01-31 22:38:13 -0300

    chore: Update Database with Stations, Connections, and Buses
    Description:
    This commit updates the database with new stations, connections, and buses. The changes include the addition of stations such as 'Plaza 25 de Mayo' and 'Av Italia 500,' connections between these stations, and buses for each line ('Linea A,' 'Linea B,' 'Linea C,' 'Linea D'). The foreign key checks were temporarily disabled during this update.
    
    Changes:
    - Inserted new stations into the 'Stations' table.
    - Created connections between stations in the 'Connections' table.
    - Added buses for different lines in the 'Buses' table.

--- src/main/resources/sqlScripts/run.sql
+++ src/main/resources/sqlScripts/run.sql
@@ -1,61 +1,60 @@
 SET FOREIGN_KEY_CHECKS=0;
 
--- Assuming navigator is the name of your database
-USE navigator;
+-- Insert stations
+INSERT INTO Stations (station_name, coordinate_x, coordinate_y) VALUES
+('Plaza 25 de Mayo', -27.451132, -58.986606),
+('Av Sarmiento 500', -27.447226, -58.982173),
+('Plaza 12 de Octubre', -27.443479, -58.986483),
+('Av 25 de mayo 500', -27.447240, -58.990845),
+('Plaza 9 de Julio', -27.451174, -58.995239),
+('Plaza Espana', -27.458902, -58.986494),
+('Av 9 de Julio 500', -27.455005, -58.982158),
+('Av Italia 500', -27.451271, -58.977913),
+('Av Hernandariaz 0', -27.444982, -58.993654);
 
--- Insert data into Stations table
-INSERT INTO navigator.Stations (id, station_name, coordinate_x, coordinate_y)
-VALUES
-    (1, 'Shell Station Buenos Aires', -34.615852, -58.417758),
-    (2, 'A3C M6, Voložin', 53.899161, 27.566893),
-    (3, 'YPF Station Cordoba', -31.420083, -64.188776),
-    (4, 'A3C M10 Vobrub', 53.915647, 27.532477),
-    (5, 'YPF Station Buenos Aires', -34.607568, -58.384572),
-    (6, 'A3C Station Minsk', 53.870503, 27.586374),
-    (7, 'Shell Station Cordoba', -31.431256, -64.193020);
 
--- Insert data into Connections table
-INSERT INTO navigator.Connections (id, origin_station_id, destination_station_id)
-VALUES
-    (1, 1, 2),
-    (2, 2, 3),
-    (3, 3, 1),
-    (4, 1, 3),
-    (5, 3, 2),
-    (6, 2, 1),
-    (7, 4, 5),
-    (8, 5, 6),
-    (9, 6, 7),
-    (10, 7, 4);
+-- Insert connections between stations
+INSERT INTO Connections (origin_station_id, destination_station_id) VALUES
+(1, 4),
+(1, 2),
+(1, 7),
+(2, 1),
+(2, 3),
+(2, 8),
+(3, 2),
+(3, 4),
+(4, 1),
+(4, 9),
+(4, 3),
+(5, 6),
+(6, 5),
+(6, 7),
+(7, 8),
+(7, 6),
+(7, 1),
+(8, 7),
+(8, 2),
+(9, 4);
 
--- Insert data into Buses table
-INSERT INTO navigator.Buses (id, line_name)
-VALUES
-    (1, 'BusLine1'),
-    (2, 'BusLine2'),
-    (3, 'BusLine3'),
-    (4, 'Busline4');
-
--- Insert data into Routes table
-INSERT INTO Routes (id, bus_id, route_order, station_id)
-VALUES
-    (1, 1, 1, 1),
-    (2, 1, 2, 2),
-    (3, 1, 3, 3),
-    (4, 2, 1, 5),
-    (5, 2, 2, 1),
-    (6, 3, 1, 6),
-    (7, 3, 2, 7),
-    (8, 4, 3, 4),
-    (9, 4, 4, 1),
-    (10, 1, 6, 2),
-    (11, 2, 5, 7),
-    (12, 3, 5, 1),
-    (13, 4, 6, 2),
-    (14, 1, 4, 3),
-    (15, 2, 2, 5),
-    (16, 3, 4, 7),
-    (17, 4, 3, 6),
-    (18, 1, 5, 3),
-    (19, 2, 4, 2),
-    (20, 3, 3, 7);
+-- Insert buses for each line
+INSERT INTO Buses (line_name, conexion_id) VALUES
+('Linea A', 10),
+('Linea A', 20),
+('Linea A', 1),
+('Linea A', 9),
+('Linea A', 3),
+('Linea A', 17),
+('Linea A', 15),
+('Linea A', 18),
+('Linea B', 2),
+('Linea B', 4),
+('Linea B', 6),
+('Linea B', 19),
+('Linea C', 8),
+('Linea C', 11),
+('Linea C', 7),
+('Linea C', 5),
+('Linea D', 16),
+('Linea D', 14),
+('Linea D', 12),
+('Linea D', 13);
